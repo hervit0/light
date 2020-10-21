c21e38c8c0538b4ddcf4b8115b417ac18689412e
Hervinho <hervito.dela.vega@gmail.com>
2020-10-21 22:01:34 +0000

    init

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rigidbody;
+
+    void FixedUpdate() {
+        float x = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(x * speed, rigidbody.velocity.y);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
