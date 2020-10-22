3181906a2863b78a70e36ec9a699bdbc90e65421
Hervinho <hervito.dela.vega@gmail.com>
2020-10-22 22:39:02 +0000

    Save work

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -6,20 +6,35 @@ public class Hero : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rigidbody;
+    public float jumpForce;
+    public SpriteRenderer spriteRenderer;
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         float x = Input.GetAxis("Horizontal");
         rigidbody.velocity = new Vector2(x * speed, rigidbody.velocity.y);
-    }
-
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded())
+        {
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if (rigidbody.velocity.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (rigidbody.velocity.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, -0.1f, 0), Vector2.down, 0.2f);
+        return hit.collider != null;
     }
 }
